e2d40179e9f692168be7cfbd9f0a741ccdcbcaa7
Sam <kulkarni.samarth@yahoo.com>
2023-04-06 17:00:19 -0400

    Completed Steps 1 and 2
    Near-Finalized version of Speech To Text and Video to Audio
    Transcription file included as sample for reference

--- OpenAI-API/SpeechToText.py
+++ OpenAI-API/SpeechToText.py
@@ -10,9 +10,14 @@ load_dotenv(dotenv_path=path)
 openai.organization = os.getenv('organization')
 openai.api_key = os.getenv("api_key")
 
-audio_file = open("D:\OpenAI-Api-Shenanigans\OpenAI-API\output_audio_15.mp3", "rb")
 
-transcription = openai.Audio.transcribe("whisper-1", audio_file)
+def transcribe(file_path):
+    audio_file = open(file_path + '\output_audio.mp3', "rb")
 
-with open('transcription.txt', 'w') as f:
-    f.write(transcription['text'])
+    transcription = openai.Audio.transcribe("whisper-1", audio_file)
+
+    with open('transcription.txt', 'w') as f:
+        f.write(transcription['text'])
+
+
+transcribe("D:\Random Files")
--- OpenAI-API/video_to_audio.py
+++ OpenAI-API/video_to_audio.py
@@ -1,32 +1,46 @@
 import moviepy.editor as mp
 from pydub import AudioSegment
 import os
-from pathlib import Path
-from dotenv import load_dotenv
-
-path = Path("Environment-Variables/.env")
-load_dotenv(dotenv_path=path)
 
 
-def get_audio_from_video():
+def get_audio_from_video(path):
     # Load Video File
-    clip = mp.VideoFileClip(r"D:\Random Files\sample_video.mp4")
+    clip = mp.VideoFileClip(r"" + path + "\input_video.mp4")
     # Write the audio track to a mp3
-    clip.audio.write_audiofile(r"D:\Random Files\output_audio.mp3")
-    print("The audio has been stored in 'output_audio.mp3'")
+    base_bitrate = 65
+    clip.audio.write_audiofile(r"" + path + "\output_audio.mp3", bitrate=str(base_bitrate) + 'k')
+
+    audio_compressor(path, clip, base_bitrate)
+    print("Conversion Successfully Completed")
 
 
-def fetch_first_5_minutes():
-    song = AudioSegment.from_mp3("D:\Random Files\output_audio.mp3")
+def audio_compressor(path, clip, bitrate):
+    reduction_factor = 1
+    while check_file_size(path):
+        bitrate = int(bitrate / reduction_factor)
+        clip.audio.write_audiofile(r"" + path + "\output_audio.mp3", bitrate=str(bitrate) + 'k')
+        reduction_factor += 1
+
+
+def fetch_first_5_minutes(path):
+    audio = AudioSegment.from_mp3(path + "\output_audio.mp3")
     ten_minutes = 15 * 60 * 1000
-    first_10_minutes = song[:ten_minutes]
-    first_10_minutes.export("D:\Random Files\output_audio_15.mp3", format="mp3")
+    first_10_minutes = audio[:ten_minutes]
+    first_10_minutes.export(path + "\output_audio_15.mp3", format="mp3")
 
 
-def get_filepath():
-    here = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(here, 'output_audio.mp3')
-    return filename
+def check_file_size(path):
+    file = path + "\output_audio.mp3"
+    stats = os.stat(file)
+
+    if (stats.st_size / (1024 * 1024)) > 25:
+        return True
+    else:
+        return False
 
 
-fetch_first_5_minutes()
+def video_to_audio_handler(path):
+    pass
+
+
+get_audio_from_video("D:\Random Files")
