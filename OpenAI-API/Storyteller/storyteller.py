a5537bd442bcace4ad5bd542e5fbe044d4aed699
Sam <kulkarni.samarth@yahoo.com>
2023-11-09 16:03:11 -0500

    Set up the initial prompt
    Created a starting state that can respond to any of the prompts from the file

--- OpenAI-API/Storyteller/storyteller.py
+++ OpenAI-API/Storyteller/storyteller.py
@@ -2,21 +2,33 @@ import os
 from pathlib import Path
 import file_operations as fo
 from dotenv import load_dotenv
-import openai
+from openai import OpenAI
 
 # Get environment variables
 path = Path("Environment-Variables/.env")
 load_dotenv(dotenv_path=path)
 
-# Setting organization and API keys
-openai.organization = os.getenv('organization')
-openai.api_key = os.getenv("api_key")
+# Setup OpenAI client
+client = OpenAI(
+    organization=os.getenv('organization'),
+    api_key=os.getenv("api_key")
+)
 
 # Get category
 category = input("What category would you like to generate a story from? ")
 
 # Get a random prompt from the category
 prompt = fo.read_category(category)
+prompt = "Use the following prompt to generate an interactive story. Ask a question at the end of each response, and let the user respond with what they would do: " + prompt
+conversation_history = [prompt]
 
 # Set up the starting GPT prompt
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompt}
+    ]
+)
 
+# Print the response
+print(response.choices[0].message)
