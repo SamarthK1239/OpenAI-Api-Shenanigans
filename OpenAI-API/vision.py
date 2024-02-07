00a6724b76b9f960662839148021a557c11f8755
Samarth Kulkarni <kulkarni.samarth@yahoo.com>
2024-02-06 22:56:59 -0500

    Template use of Vision Preview
    Not entirely sure what I want to do with it, but now I kinda sorta maybe know how it works

---
+++ OpenAI-API/vision.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Get environment variables
+path = Path("Environment-Variables/.env")
+load_dotenv(dotenv_path=path)
+
+# Set up openai client
+openai = OpenAI(
+    organization=os.getenv('organization'),
+    api_key=os.getenv("api_key")
+)
+
+response = openai.chat.completions.create(
+    model="gpt-4-vision-preview",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What's in this image?"},
+                {"type": "image_url",
+                 "image_url": "https://images.unsplash.com/photo-1706034381055-d176c40693d3?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+            ]
+        }
+    ],
+    max_tokens=500
+)
+
+print(response.choices[0])
